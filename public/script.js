149c31eba9ff5a29dd3b1220cce330be8a288f2d
Cordova <cordovansyah@gmail.com>
2022-11-18 11:46:53 +0700

    Define model architecture

--- public/script.js
+++ public/script.js
@@ -1,65 +1,82 @@
-
 /* ------------------------------------------------------------------------
 1. GET CAR DATA & REQUIRED VARIABLES
 */
 async function getData() {
-    const carsDataRawSource = await fetch(
-      "https://storage.googleapis.com/tfjs-tutorials/carsData.json"
-    );
-    const carsDataProcessing = await carsDataRawSource.json();
-    const carsDataClaned = carsDataProcessing
-      .map((car) => ({
-        kmpg: car.Miles_per_Gallon * 1.6,
-        horsepower: car.Horsepower,
-      }))
-      //Remove any pre-defined data that doesn't contain kmpg and horsepower data units
-      .filter((car) => car.kmpg != null && car.horsepower != null);
-    return carsDataClaned;
-  }
-  
-  /* ------------------------------------------------------------------------
-  2. DATA VISUALIZATION
-  */
-  async function visualize() {
-    const data = await getData();
-    const values = data.map((d) => ({
-      x: d.kmpg,
-      y: d.horsepower,
-    }));
+  const carsDataRawSource = await fetch(
+    "https://storage.googleapis.com/tfjs-tutorials/carsData.json"
+  );
+  const carsDataProcessing = await carsDataRawSource.json();
+  const carsDataClaned = carsDataProcessing
+    .map((car) => ({
+      kmpg: car.Miles_per_Gallon * 1.6,
+      horsepower: car.Horsepower,
+    }))
+    //Remove any pre-defined data that doesn't contain kmpg and horsepower data units
+    .filter((car) => car.kmpg != null && car.horsepower != null);
+  return carsDataClaned;
+}
 
-    //Render Data
-    var options = {
-        series: [{
+/* ------------------------------------------------------------------------
+  2. DATA VISUALIZATION
+*/
+async function visualize() {
+  const data = await getData();
+  const values = data.map((d) => ({
+    x: d.kmpg,
+    y: d.horsepower,
+  }));
+
+  //Render Data
+  var options = {
+    series: [
+      {
         name: "X: Kilometer per Gallon • Y: Horsepower",
-        data: values 
-        }],
-      chart: {
-        height: 700,
-        type: 'scatter', 
-        zoom: {
-          enabled: true,
-          type: 'xy'
-        }
+        data: values,
       },
-      xaxis: {
-        tickAmount: 10, 
-        labels: {
-          formatter: function(val) {
-            return parseFloat(val).toFixed(1)
-          }
-        }
+    ],
+
+    chart: {
+      height: 700,
+      type: "scatter",
+      zoom: {
+        enabled: true,
+        type: "xy",
       },
-      yaxis: {
-        tickAmount: 7
+    },
+    xaxis: {
+      tickAmount: 10,
+      type: "category",
+      labels: {
+        formatter: function (val) {
+          return parseFloat(val).toFixed(1);
+        },
       },
-      title: {
-        text: 'Horsepower v Kilometer per Gallon',
-        align: 'center'
-      },
-    };
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
-  }
+    },
+    yaxis: {
+      tickAmount: 7,
+    },
+    title: {
+      text: "Horsepower v Kilometer per Gallon",
+      align: "center",
+    },
+  };
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+}
 visualize();
 
+/* ------------------------------------------------------------------------
+  3. DEFINE MODEL ARCHITECTURE & CREATE THE MODEL
+*/
+
+function createModel() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true })); //Input layer
+  model.add(tf.layers.dense({ units: 1, useBias: true })); //Output layer
+
+  return model;
+}
+
+const model = createModel();
+tfvis.show.modelSummary({name: 'Model Summary'}, model);
 
